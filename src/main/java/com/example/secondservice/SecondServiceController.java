6c17155839b274058c9db84d67c464d4ccd1d3f4
Hwangwonuk <hok0173@naver.com>
2023-01-31 13:48:47 +0900

    Spring Cloud Gateway - 프로젝트 생성

--- src/main/java/com/example/secondservice/SecondServiceController.java
+++ src/main/java/com/example/secondservice/SecondServiceController.java
@@ -14,15 +14,4 @@ public class SecondServiceController {
     public String welcome() {
         return "Welcome to the Second service.";
     }
-
-    @GetMapping("/message")
-    public String message(@RequestHeader("second-request") String header) {
-        log.info(header);
-        return "Hello World in Second Service.";
-    }
-
-    @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message from Second Service.";
-    }
 }
